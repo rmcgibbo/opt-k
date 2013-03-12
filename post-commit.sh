f5da8791c2123cb8b49cb8a82875f1e545342ed8
Robert McGibbon <rmcgibbo@gmail.com>
2013-03-12 13:20:10 -0700

    stuff

--- post-commit.sh
+++ post-commit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #http://stackoverflow.com/questions/3284292/can-a-git-hook-automatically-add-files-to-the-commit
-echo "post commit hook"
-if [ -a "$(git rev-parse --show-toplevel)/.commit" ] then
+if [ -a "$(git rev-parse --show-toplevel)/.commit" ]
+then
     rm "$(git rev-parse --show-toplevel)/.commit"
     git add paper/README.md
-    git commit --ammend -C HEAD --no-verify
+    git commit --amend -C HEAD --no-verify
     echo "post commit hook"
 fi
