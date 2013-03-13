47a8889803dff42934ddcd2e367d35352af337c3
Christian Schwantes <schwancr@stanford.edu>
2013-03-12 18:04:10 -0700

    Added directory to hold the muller test cases. Also changed MonteCarlo to reject points that are not 'close enough' to the data. The way I defined that was whether that point is closer than some cutoff (cushion) to a set of space_generators, that can be the same as the clustering generators or different (e.g., the whole dataset, or a different clustering's generators).

---
+++ muller/simulate.py
@@ -0,0 +1,23 @@
+import random
+from mullermsm import muller
+mullerforce = muller.muller_force()
+from sklearn import mixture
+import scipy.linalg
+from matplotlib.pyplot import *
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('--dt', dest='dt', type=float, default=0.1)
+parser.add_argument('-n', dest='num_frames', type=int, default=100000)
+parser.add_argument('-o', dest='output', default='pos.npy')
+
+args = parser.parse_args()
+
+kT = 15.0
+dt = args.dt
+mGamma = 1000.0
+traj_length = args.num_frames 
+initial_x = [random.uniform(-1.5, 1.2), random.uniform(-0.2, 2)]
+positions = muller.propagate(traj_length, initial_x, kT, dt, mGamma, mullerforce)
+
+np.save(args.output, positions)
