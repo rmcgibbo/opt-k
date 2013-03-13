d975a040b18961f982b5f098d9712e4492fd549e
Christian Schwantes <schwancr@stanford.edu>
2013-03-13 11:26:32 -0700

    Added likelihoods for 1-100 + 1000 state models

---
+++ muller/MSMs/get_like.py
@@ -0,0 +1,18 @@
+import likelihood
+import numpy as np
+from msmbuilder import io
+from scipy.io import mmread
+
+
+Ls = []
+Ls.append(0) # for a one state model the likelihood is 0
+
+for k in range(2, 101) + [ 1000 ]:
+    print k
+    t = mmread('k%d/Lag30/tProb.mtx' % k)
+    v = np.load('k%d/vols.npy' % k)
+    a = io.loadh('k%d/Assignments.h5' % k)
+    a = a['arr_0']
+    Ls.append(likelihood.get_log_likelihood(t, a, v, lagtime=30))
+
+np.save('likes.npy', np.array(Ls))
