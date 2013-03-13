6987786b47b6cefda171efdaad11d200585cd83a
Christian Schwantes <schwancr@stanford.edu>
2013-03-13 11:21:20 -0700

    Bugfix in likelihood.py The problem was that scipy.sparse matrices use np.matrices as opposed to np.arrays, so when I was using the '*' operator it was doing matrix multiplication when I really wanted element-wise multiplication. The solution was to cast everything to arrays so that it is obvious what is going on.

--- lib/likelihood.py
+++ lib/likelihood.py
@@ -12,7 +12,7 @@ def get_log_likelihood(tProb, trajs, volumes, lagtime=1):
     Function to calculate the log likelihood according to:
 
     .. math:: \log L = \sum_{i,j} C_{ij} \log T_{ij} - 
-        \sum_{i,j} c_i \log V_i
+        \sum_{i} c_i \log V_i
 
     where :math:`C_{ij}` is the number of transitions from state i to state
     j, :math:`T_{ij}` is the probability of transistioning from state i to
@@ -38,14 +38,21 @@ def get_log_likelihood(tProb, trajs, volumes, lagtime=1):
 
     counts = MSMLib.get_count_matrix_from_assignments(trajs, lag_time=lagtime)
 
-
+    counts = counts.tocsr()
     tProb = tProb.tocsr()
-    counts.eliminate_zeros()
-    state_counts = counts.sum(axis=1)
 
     ij = counts.nonzero()
 
-    log_like = np.sum(np.array(counts.data) * np.array(np.log(tProb[ij]))) - \
-        np.sum(state_counts * np.log(volumes))
+    state_counts = np.array(counts.sum(axis=1)).flatten()
+    nonzero_counts = np.array(counts.data).flatten()
+    nonzero_tprobs = np.array(tProb[ij]).flatten()
+    volumes = np.array(volumes).flatten()
+    # scipy.sparse uses np.matrices which behave differently than arrays
+    # so we cast everything to arrays and do the math with them
     
+    #log_like = np.sum(np.array(counts.data) * np.array(np.log(tProb[ij]))) - \
+    #    np.sum(state_counts * np.log(volumes))
+    log_like = np.sum(nonzero_counts * np.log(nonzero_tprobs)) - \
+        np.sum(state_counts * np.log(volumes))
+
     return log_like
