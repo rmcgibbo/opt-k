1931217fe58c8d8ed0e4a82bc3be289398ac0aa5
Christian Roger Schwantes <schwancr@stanford.edu>
2013-03-13 19:00:52 -0700

    New data generated by volumes calculated with 100,000 MC samples.

--- muller/MSMs/get_like.py
+++ muller/MSMs/get_like.py
@@ -10,9 +10,9 @@ Ls.append(0) # for a one state model the likelihood is 0
 for k in range(50,1050,50):
     print k
     t = mmread('k%d/Lag30/tProb.mtx' % k)
-    v = np.load('k%d/vols.npy' % k)
+    v = np.load('k%d/vols100k.npy' % k)
     a = io.loadh('k%d/Assignments.h5' % k)
     a = a['arr_0']
     Ls.append(likelihood.get_log_likelihood(t, a, v, lagtime=30))
 
-np.save('likes1000_s50.npy', np.array(Ls))
+np.save('likes1000_s50_n100k.npy', np.array(Ls))
