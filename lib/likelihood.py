b80d7fdf85544c89eb1ecbbb6b5814fabb488958
Christian Schwantes <schwancr@stanford.edu>
2013-11-14 17:00:20 -0800

    added a figure

--- lib/likelihood.py
+++ lib/likelihood.py
@@ -77,9 +77,8 @@ def get_log_likelihood(tProb, trajs, volumes, lagtime=1, separate=False,
     return t_like + v_like
 
 
-def get_kmeans_log_likelihood(tprob, trajs, state_dists, num_dims, lagtime=1,
-    mapping=None, add_weights=False):
-
+def get_kmeans_log_likelihood(tprob, trajs, state_dists, num_dims, 
+    lagtime=1, train_dists=None, train_ass=None, mapping=None, add_weights=False):
     """
     get the log likelihood of an MSM given that each state emits a conformation
     according to a user-input probability model. 
@@ -96,6 +95,11 @@ def get_kmeans_log_likelihood(tprob, trajs, state_dists, num_dims, lagtime=1,
         number of dimensions in your vector space
     lagtime : int, optional
         lag time of the MSM (default: 1)
+    train_dists : np.ndarray, optional
+        the distance to the cluster center for the training data. If this
+        is not None, then we assume you are doing cross-validation, 
+        which means the state variance is estimated from these distances
+        and the state_dists are used in the likelihood separately
     mapping : mapping to map counts to a transition matrix
     add_weights : bool, optional
         add the per-state weights corresponding to R_n / R from the X-means 
@@ -112,6 +116,9 @@ def get_kmeans_log_likelihood(tprob, trajs, state_dists, num_dims, lagtime=1,
         with Efficient Estimation of the Number of Clusters. 
         (http://www.cs.cmu.edu/~dpelleg/download/xmeans.pdf)
     """
+
+    if not train_dists is None and not train_ass is None:
+        cross = True
 
     if not mapping is None:
         MSMLib.apply_mapping_to_assignments(trajs, mapping)
@@ -139,10 +146,18 @@ def get_kmeans_log_likelihood(tprob, trajs, state_dists, num_dims, lagtime=1,
 
     #R = np.sum(state_counts) # total number of points
     R = np.where(trajs != -1)[0].shape[0]
-    state_var = np.square(state_dists[np.where(trajs != -1)]).sum() / (R - tprob.shape[0])
-    print state_var
+    if cross:
+        orig_R = np.where(train_ass != -1)[0].shape[0]
+        state_var = np.square(train_dists[np.where(train_ass != -1)]).sum() / (orig_R - tprob.shape[0])
+    else:
+        state_var = np.square(state_dists[np.where(trajs != -1)]).sum() / (R - tprob.shape[0])
 
-    s_like = - num_dims / 2. * R * np.log(2 * np.pi * state_var) - (R - tprob.shape[0]) / 2.
-    print t_like, s_like, t_like + s_like
+    s_like = - num_dims / 2. * R * np.log(2 * np.pi * state_var) 
+    if cross:
+        s_like -= np.square(state_dists[np.where(trajs != -1)]).sum() / (2. * state_var)
+    else:
+        s_like -= (R - tprob.shape[0]) / 2.
+        
+    print 't-like: %.4e s-like: %.4e tot: %.4e' % (t_like, s_like, t_like + s_like)
     return t_like + s_like
 
